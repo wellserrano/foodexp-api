d01e2fa6ea14b4002e4d4559fb512563d6593108
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-12-02 15:41:54 -0300

    Feat: user creation

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,17 +1,37 @@
 const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
 
+const { hash, compare } = require("bcrypt")
 class UsersControllers {
 
   index(req, res) {
     res.json({status: 'Online'})
   }
 
-  create(req, res) {
-    const { name, email, password } = req.body
+  async create(req, res) {
+    const { name, email, password, admin } = req.body
 
     if (!name ||!email ||!password) {
       throw new AppError("Name, email and password are required", 401)
     }
+
+    const userExists = await knex("users")
+      .where({ email })
+      .first();
+    
+    if (userExists) {
+      throw new AppError("User already exists", 409)
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users")
+      .insert({
+        name,
+        email,
+        admin,
+        password: hashedPassword,
+      })
 
     return res.status(201).json()
   }

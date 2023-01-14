1b00da528387e4e23906704c2ddb8b41d1df12fa
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-14 13:27:01 -0300

    ft: order routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,18 @@ const { Router } = require("express");
 const routes = Router();
 
 const statusRouter = require("./status.routes")
-const usersRouter = require("./users.routes")
-const productsRouter = require("./products.routes")
-const ingredientsRouter = require("./ingredients.routes")
 const sessionsRouter = require("./sessions.routes")
 
+const usersRouter = require("./users.routes")
+const ordersRouter = require("./orders.routes")
+const productsRouter = require("./products.routes")
+const ingredientsRouter = require("./ingredients.routes")
+
 routes.use("/status", statusRouter);
-routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
+
+routes.use("/users", usersRouter);
+routes.use("/orders", ordersRouter);
 routes.use("/products", productsRouter);
 routes.use("/ingredients", ingredientsRouter);
 

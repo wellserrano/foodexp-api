ce939a87181ecd0309685f828c1b91eadc4a4a33
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-14 12:22:11 -0300

    ft: migration for orders table

---
+++ src/database/knex/migrations/20230114143425_createOrders.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("orders", table => {
+  table.increments("id");
+  table.string("status").notNullable();
+  table.float("total").notNullable();
+  table.string("payment_method").notNullable();
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+
+});
+
+
+exports.down = knex => knex.schema.dropTable("ingredients");
+  

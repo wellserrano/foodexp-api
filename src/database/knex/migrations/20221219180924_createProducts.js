50b0af8abc5f7ba53943d97d1564d75909e8a3b5
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-17 10:13:25 -0300

    Including category column to products table migration

--- src/database/knex/migrations/20221219180924_createProducts.js
+++ src/database/knex/migrations/20221219180924_createProducts.js
@@ -4,6 +4,7 @@ exports.up = knex => knex.schema.createTable("products", table => {
   table.float("price").notNullable();
   table.string("image");
   table.string("description");
+  table.string("category");
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
 

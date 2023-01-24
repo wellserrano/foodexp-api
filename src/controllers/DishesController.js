8e3b71b2d1a5961881eadad86ae67a898cdd4427
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-24 17:58:24 -0300

    feat: sql query to join favorite data

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -3,9 +3,17 @@ const AppError = require('../utils/AppError')
 
 class DishesController {
   async index(req, res) {
-    const { category } = req.query;
+    const { category, user_id } = req.query;
 
-    const data = await knex('products').where({ category });
+    const data = await knex
+      .select('products.*', knex.raw("CASE WHEN favorites.id > 0 THEN true ELSE false END AS isFavorite"))
+      .from('products')
+      .leftJoin('favorites', function() {
+        this.on('products.id', '=', 'favorites.product_id')
+        .andOn('favorites.user_id', '=', knex.raw('?', [user_id]))
+      })
+      .where({'products.category': knex.raw('?', [category])})
+
     return res.status(200).json(data)
   }
 }

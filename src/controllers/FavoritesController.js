a987d8dc7b16e9f66f430ea85a74bd64dda24aa8
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-25 15:27:53 -0300

    handling favorites toggle

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -2,8 +2,30 @@ const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 
 class FavoritesController {
+  async index(req, res) {
+    const { id: user_id } = req.query
+    
+    const data = await knex("favorites")
+      .where({ user_id })
+
+    return res.json(data)
+  }
+
   async create(req, res) {
-    console.log('agagagas')
+    const { favoriteInfo } = req.body
+
+    const data = await knex("favorites")
+      .insert(favoriteInfo)
+
+    return res.status(200).json(data[0])
+  }
+
+  async toggleFavorite(req, res) {
+    const id = req.query.id
+
+    await knex("favorites")
+      .where({ id })
+      .del()
 
     return res.status(200).json()
   }
--- src/routes/favorites.routes.js
+++ src/routes/favorites.routes.js
@@ -5,6 +5,7 @@ const favoritesRoutes = Router();
 const FavoritesController = require("../controllers/FavoritesController")
 const favoritesController = new FavoritesController();
 
+favoritesRoutes.get("/", favoritesController.index);
 favoritesRoutes.post("/", favoritesController.create);
 favoritesRoutes.delete("/", favoritesController.toggleFavorite);
 

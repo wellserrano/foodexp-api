a37bce37378463ba5aac16c12ea167c1c5651fe0
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-31 10:33:09 -0300

    fix: table columns

--- src/database/knex/migrations/20230114143425_createOrders.js
+++ src/database/knex/migrations/20230114143425_createOrders.js
@@ -2,7 +2,6 @@ exports.up = knex => knex.schema.createTable("orders", table => {
   table.increments("id");
   table.string("status").notNullable().defaultTo("Preparando");
   table.float("total").notNullable();
-  table.string("payment_method").notNullable().defaultTo("Caixa");
   table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
